b4754b6c179eb9dce8b5ba4589ca5ec358f2d4e9
Michael Corey <corey101@umn.edu>
2023-02-19 18:39:53 -0600

    Slider version

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), viteCommonjs()]
 });
